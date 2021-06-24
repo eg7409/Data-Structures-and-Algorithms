6869966bb7073a700e25163ff6b29bbabcdaff95
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-06-24 23:39:09 +0530

    Update FizzBuzz.cpp

--- Arrays & vector/FizzBuzz.cpp
+++ Arrays & vector/FizzBuzz.cpp
@@ -1,1 +1,33 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> FizzBuzz(int n){
+
+    vector<string> result;
+
+    for(int i=1;i<=n;i++){
+        if(i%15==0){
+            result.push_back("FizzBuzz");
+        }
+        else if(i%5==0){
+            result.push_back("Buzz");
+        }
+        else if(i%3==0){
+            result.push_back("Fizz");
+        }
+        else{
+            result.push_back(to_string(i));
+        }
+    }
+    return result;
+}
+int main(){
+
+   vector<string> v = FizzBuzz(30);
+   for(auto i : v){
+       cout<<i<<" ";
+   }
+   cout<<endl;
+	return 0;
+}
