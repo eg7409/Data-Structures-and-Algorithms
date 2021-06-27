daee986f017132a44d929da3d0af8372af0d4a6e
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-06-28 00:21:17 +0530

    Update Housing Implementation + smallest window.cpp

--- Sliding Window Problems/Housing Implementation + smallest window.cpp
+++ Sliding Window Problems/Housing Implementation + smallest window.cpp
@@ -41,3 +41,53 @@ int main(){
   cout<<f<<" "<<s<<endl;
   return 0;
 }
+
+
+
+
+// STORING ONLY ONE VALUE IN VECTOR
+#include<bits/stdc++.h>
+using namespace std;
+
+//Housing Implementation + smallest window
+
+vector<pair<int,int> > sliding(vector<int> v,int target){
+  int n = v.size();
+  vector<pair<int,int> > p;
+
+  int i=0,j=0;
+  int sum = 0;
+  int f=0,s=0,find = INT_MAX;
+
+  while(j<n){
+    sum += v[j];
+    j++;
+
+    while(sum>target && i<j){
+      sum -= v[i];
+      i++;
+    }
+    if(sum == target){
+      if(find>j-1-i){
+        find = j-1-i;
+        f = i;
+        s = j-1;
+      }
+      
+    }
+  }
+  p.push_back({f,s});
+  return p;
+}
+
+int main(){
+  
+  vector<pair<int,int> > v = sliding({1,3,2,1,4,1,3,2,1,1},8);
+  
+
+  for(auto i : v){
+    cout<<i.first<<" "<<i.second<<endl;
+  }
+  
+  return 0;
+}
