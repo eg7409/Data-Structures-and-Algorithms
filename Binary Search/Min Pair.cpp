b40f5a93b26f4316b3a48f4840a1c5766fc3f24b
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-07-09 02:39:16 +0530

    Update Min Pair.cpp

--- Binary Search/Min Pair.cpp
+++ Binary Search/Min Pair.cpp
@@ -1,61 +1,70 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void min_pair(vector<int> v1,vector<int> v2){
+
+//Min Pair
+void min_pair(vector<int> v1, vector<int> v2)
+{
 
 	int n = v1.size();
 	int diff = INT_MAX;
-	int left = -1,right = -1;
+	int left = -1, right = -1;
 
-	sort(v2.begin(),v2.end());
+	sort(v2.begin(), v2.end());
 
-	for(int i=0;i<n;i++){
-		auto it = lower_bound(v2.begin(),v2.end(),v1[i]) - v2.begin();
+	for (int i = 0; i < n; i++)
+	{
+		auto it = lower_bound(v2.begin(), v2.end(), v1[i]) - v2.begin();
 
-		if(it == 0){
-			if(abs(v2[it] - v1[i]) < diff){
+		if (it == 0)
+		{
+			if (abs(v2[it] - v1[i]) < diff)
+			{
 				diff = abs(v2[it] - v1[i]);
 				left = v1[i];
 				right = v2[it];
-				
 			}
 		}
-		else if(it == v2.size()){
-			if(abs(v2[it-1] - v1[i]) < diff){
-				diff = abs(v2[it-1] - v1[i]);
+		else if (it == v2.size())
+		{
+			if (abs(v2[it - 1] - v1[i]) < diff)
+			{
+				diff = abs(v2[it - 1] - v1[i]);
 				left = v1[i];
-				right = v2[it-1];
-				
+				right = v2[it - 1];
 			}
 		}
-		else{
-			if(abs(v2[it] - v1[i]) < (abs(v2[it-1] - v1[i]))){
-				if(abs(v2[it] - v1[i]) < diff){
+		else
+		{
+			if (abs(v2[it] - v1[i]) < (abs(v2[it - 1] - v1[i])))
+			{
+				if (abs(v2[it] - v1[i]) < diff)
+				{
 					diff = abs(v2[it] - v1[i]);
-				    left = v1[i];
-			    	right = v2[it];
-					
-		    	}
+					left = v1[i];
+					right = v2[it];
+				}
 			}
-			else{
-				if(abs(v2[it-1] - v1[i]) < diff){
-					diff = abs(v2[it-1] - v1[i]);
-			    	left = v1[i];
-				    right = v2[it-1];
-					
-			    }
+			else
+			{
+				if (abs(v2[it - 1] - v1[i]) < diff)
+				{
+					diff = abs(v2[it - 1] - v1[i]);
+					left = v1[i];
+					right = v2[it - 1];
+				}
 			}
 		}
 	}
-	cout<<left<<" "<<right<<endl;
+	cout << left << " " << right << endl;
 }
 
-
-int  main(){
+int main()
+{
 
 	vector<int> a1 = {-1, 5, 10, 20, 3};
-	vector<int> a2 = { 15, 17};
-	min_pair(a1,a2);
+	vector<int> a2 = {26, 134, 135, 15, 17};
+	min_pair(a1, a2);
 
 	return 0;
 }
