74445b8bd704ac7f7ee4df8f686c0d792cc02ce5
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-07-09 02:37:31 +0530

    Update Min Pair.cpp

--- Binary Search/Min Pair.cpp
+++ Binary Search/Min Pair.cpp
@@ -1,64 +1,61 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-//Min Pair
+void min_pair(vector<int> v1,vector<int> v2){
 
-pair<int, int> solve(vector<int> v1, vector<int> v2, int n, int m)
-{
+	int n = v1.size();
+	int diff = INT_MAX;
+	int left = -1,right = -1;
 
-    sort(v2.begin(), v2.end());
-    int mini = INT_MAX;
-    int left = 0, right = 0;
+	sort(v2.begin(),v2.end());
 
-    for (int i = 0; i < n; i++)
-    {
-        auto it = lower_bound(v2.begin(), v2.end(), v1[i]);
+	for(int i=0;i<n;i++){
+		auto it = lower_bound(v2.begin(),v2.end(),v1[i]) - v2.begin();
 
-        if (it != v2.end())
-        {
-            if (it - v2.begin() == 0)
-            {
-                if (mini > (abs(*it - v1[i])))
-                {
-                    mini = (abs(*it - v1[i]));
-                    left = v1[i];
-                    right = *it;
-                }
-            }
-            else
-            {
-                if (mini > (abs(*it - v1[i]) || mini > abs(v2[it - v2.begin() - 1] - v1[i])))
-                {
-
-                    if (abs(*it - v1[i]) < abs(*(it - 1) - v1[i]))
-                    {
-                        mini = min(abs(*it - v1[i]), abs(v2[it - v2.begin() - 1] - v1[i]));
-                        left = v1[i];
-                        right = *it;
-                    }
-                    else
-                    {
-                        mini = min(abs(*it - v1[i]), abs(v2[it - v2.begin() - 1] - v1[i]));
-                        left = v1[i];
-                        right = v2[it - v2.begin() - 1];
-                    }
-                }
-            }
-        }
-        //cout<<*it<<" "<<v1[i]<<" "<<v2[it-v2.begin()-1]<<" " <<mini<<endl;
-    }
-    return {left, right};
+		if(it == 0){
+			if(abs(v2[it] - v1[i]) < diff){
+				diff = abs(v2[it] - v1[i]);
+				left = v1[i];
+				right = v2[it];
+				
+			}
+		}
+		else if(it == v2.size()){
+			if(abs(v2[it-1] - v1[i]) < diff){
+				diff = abs(v2[it-1] - v1[i]);
+				left = v1[i];
+				right = v2[it-1];
+				
+			}
+		}
+		else{
+			if(abs(v2[it] - v1[i]) < (abs(v2[it-1] - v1[i]))){
+				if(abs(v2[it] - v1[i]) < diff){
+					diff = abs(v2[it] - v1[i]);
+				    left = v1[i];
+			    	right = v2[it];
+					
+		    	}
+			}
+			else{
+				if(abs(v2[it-1] - v1[i]) < diff){
+					diff = abs(v2[it-1] - v1[i]);
+			    	left = v1[i];
+				    right = v2[it-1];
+					
+			    }
+			}
+		}
+	}
+	cout<<left<<" "<<right<<endl;
 }
-int main()
-{
 
-    vector<int> v1 = {-1, 5, 10, 20, 3};
-    vector<int> v2 = {26, 134, 135, 15, 17};
 
-    int n = v1.size(), m = v2.size();
+int  main(){
 
-    pair<int, int> p = solve(v1, v2, n, m);
+	vector<int> a1 = {-1, 5, 10, 20, 3};
+	vector<int> a2 = { 15, 17};
+	min_pair(a1,a2);
 
-    cout << p.first << " " << p.second << endl;
-    return 0;
+	return 0;
 }
