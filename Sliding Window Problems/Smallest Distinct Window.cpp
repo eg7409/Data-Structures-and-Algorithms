0e0e59d6de6921243870931909c9603d3313c5e3
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-07-26 02:38:57 +0530

    Update Smallest Distinct Window.cpp

--- Sliding Window Problems/Smallest Distinct Window.cpp
+++ Sliding Window Problems/Smallest Distinct Window.cpp
@@ -30,3 +30,55 @@ string smallestWindow(string s){
     }
     return s.substr(index,maxi);
 }
+/*
+(same concept)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Smallest Distinct Window
+
+string solve(string s){
+
+    int n = s.length();
+
+    map<char,int> m;
+
+    int i=0;
+    int length=0,maxLength=0,index=0;
+
+    for(int j=0;j<n;j++){
+
+        auto it = m.find(s[j]);
+        if(it!=m.end()){
+            if(m[s[j]] >= i){
+                length = j-i;
+                if(maxLength<length){
+                    maxLength = length;
+                    index = i;
+                }
+                i = m[s[j]] + 1;
+                m[s[j]] = j;
+            }
+            else{
+                m[s[j]] = j;
+            }
+        }
+        else{
+                m[s[j]] = j;
+            }
+            
+    }
+  
+    return s.substr(index,maxLength);
+}
+
+int main(){
+
+   
+    string s = "aabcbcdbcaaad";
+    cout<<solve(s);
+
+    return 0;
+}
+*/
