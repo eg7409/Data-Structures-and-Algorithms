02b69c3a8a1f0626c0d4fe64eaef701085827830
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-07-27 16:35:38 +0530

    Update Staircase Search.cpp

--- Sorting & Searching/Staircase Search.cpp
+++ Sorting & Searching/Staircase Search.cpp
@@ -5,12 +5,31 @@ using namespace std;
 pair<int,int> search(int m, int n, vector<vector<int>> v, int k){
     //write your code here.
     
-    for(int i=0;i<n;i++){
+    /*for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             if(v[i][j] == k){
                 return {i,j};
             }
         }
     }
+    return {-1,-1};*/
+    
+  
+    int index = -1;
+    
+    for(int i=0;i<n;i++){
+        if(v[0][i] == k){
+            return {0,i};
+        }
+        if(v[0][i] < k){
+            index = i;
+        }
+    }
+    for(int i=0;i<m;i++){
+        if(v[i][index] == k){
+            return {i,index};
+        }
+    }
     return {-1,-1};
+
 }
