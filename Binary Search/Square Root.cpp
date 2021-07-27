bde6860c43069da45a922d00145e4c6516107683
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-07-28 02:21:30 +0530

    Update Square Root.cpp

--- Binary Search/Square Root.cpp
+++ Binary Search/Square Root.cpp
@@ -37,3 +37,45 @@ int main(){
     cout<<squareroot(10,3);
   return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+float root(int n,int p){
+
+    int start = 0,end = n;
+    float ans = 0;
+
+    while(start <= end){
+        int mid = (start+end)/2;
+        if((mid*mid) <= n){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+
+        if((mid*mid) <= n){
+            ans = mid;
+        }
+    }
+
+    float point = 0.1;
+
+    for(int i=0;i<p;i++){
+        while(ans*ans < n){
+            ans+=point;
+        }
+        ans -= point;
+        point/=10;
+    }
+    return ans;
+}
+
+int main(){
+
+    cout<<root(10,3);
+    return 0;
+}
+*/
