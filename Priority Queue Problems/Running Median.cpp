66f1a895a635992407f4e66567219dabb7820332
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-09-06 00:46:15 +0530

    Create Running Median.cpp

---
+++ Priority Queue Problems/Running Median.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    priority_queue<int> max_heap;
+    priority_queue<int,vector<int> , greater<int> > min_heap;
+
+    int d;
+    cin>>d;
+
+    max_heap.push(d);
+    float median = d;
+    cout<<median<<" ";
+
+
+    cin>>d;
+
+    while(d!=-1){
+
+        if(max_heap.size() > min_heap.size()){
+            if(d > median){
+                min_heap.push(d);
+            }
+            else{
+                min_heap.push(max_heap.top());
+                max_heap.pop();
+                max_heap.push(d);
+            }
+            median = (max_heap.top() + min_heap.top())/2.0;
+        }
+        else if(max_heap.size() < min_heap.size()){
+            if(d < median){
+                max_heap.push(d);
+            }
+            else{
+                max_heap.push(min_heap.top());
+                min_heap.pop();
+                min_heap.push(d);
+            }
+            median = (max_heap.top() + min_heap.top())/2.0;
+        }
+        else{
+            if(d < median){
+                max_heap.push(d);
+                median = max_heap.top();
+            }
+            else{
+                min_heap.push(d);
+                median = min_heap.top();
+            }
+        }
+        cout<<median<<" ";
+        cin>>d;
+    }
+
+    return 0;
+}
