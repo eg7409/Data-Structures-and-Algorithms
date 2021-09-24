11c3e8c7c6e0c045fbb8d8efbc1a61e0bc62a107
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-09-25 02:53:01 +0530

    Create Coin Change.cpp

---
+++ Dynamic Programming (1D)/Coin Change.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Coin Change
+
+void solve(int target,vector<int> v){
+
+    int n = v.size();
+
+    vector<int> dp(target+1,0);
+
+    dp[0] = 0;
+
+    for(int i=1;i<=target;i++){
+        dp[i] = INT_MAX;
+        for(int c : v){
+            if(i-c>=0 && dp[i-c]!=INT_MAX){
+                dp[i] = min(dp[i],dp[i-c]) + 1;
+            }
+        }
+    }
+    if(dp[target]==INT_MAX){
+        cout<<-1<<endl;
+        return;
+    }
+    cout<<dp[target]<<endl;
+}
+
+int main(){
+
+    int target = 8;
+    vector<int> v = {1,5,7,10};
+
+    solve(target,v);
+    return 0;
+}
