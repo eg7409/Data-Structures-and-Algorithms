2473eb9f2eb4f701f887fa1008a0203aa1f93b76
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-09-25 01:59:51 +0530

    Top Down-Bottom up-optimized

--- Dynamic Programming (1D)/N-K Ladders/N-K Ladders _.cpp
+++ Dynamic Programming (1D)/N-K Ladders/N-K Ladders _.cpp
@@ -29,6 +29,41 @@ int countWays(int n,int k,vector<int> dp){
 
 }
 
+void countWays1(int n,int k){
+
+    vector<int> dp(n+1,0);
+    dp[0] = 1;
+
+
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=k;j++){
+            if(i - j >= 0){
+                dp[i] += dp[i-j];
+            }
+        }
+    }
+
+    cout<<dp[n]<<endl;
+}
+
+void solve(int n,int k){
+    vector<int> dp(n+1,0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for(int i=2;i<=k;i++){
+        dp[i] = 2*dp[i-1];
+    }
+
+    for(int i=k+1;i<=n;i++){
+        dp[i] = 2*dp[i-1] - dp[n-k-1];
+    }
+
+    cout<<dp[n]<<endl;
+}
+
+
 int main(){
 
     int n,k;
@@ -37,6 +72,8 @@ int main(){
     vector<int> dp(n+1,0);
 
     cout<<countWays(n,k,dp);
+    countWays1(6,4);
+    solve(6,4);
 
     return 0;
 }
