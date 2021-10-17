5cb7fd24c37d3de4460f9913a3fd45612936f6ee
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-10-18 01:08:10 +0530

    New Code added

--- Graphs Problem Solving/Largest Island.cpp
+++ Graphs Problem Solving/Largest Island.cpp
@@ -51,3 +51,37 @@ int largest_island(vector<vector<int> > v){
     }
     return ans;
 }
+
+/*---------------------------------------------------------------------------------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<vector<int> > v,vector<vector<bool> > &visited,int i,int j,int n,int m){
+        
+        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || v[i][j] == 0){
+            return 0;
+        }
+        
+        visited[i][j] = true;
+        
+        return (1 + solve(v,visited,i+1,j,n,m) + solve(v,visited,i-1,j,n,m) + solve(v,visited,i,j+1,n,m) +solve(v,visited,i,j-1,n,m));
+    }
+
+int largest_island(vector<vector<int> > v){
+    //return the size of largest island in grid
+    
+    int n = v.size();
+        int m = v[0].size();
+        
+        int ans = 0;
+        
+        vector<vector<bool> > visited(n,vector<bool>(m,false));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                 ans = max(ans,solve(v,visited,i,j,n,m));
+            }
+        }
+        return ans;
+}
