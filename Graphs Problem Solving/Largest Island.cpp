59f085073c8c14ace5cab2c2380927b0f6079101
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-10-18 00:46:20 +0530

    Create Largest Island.cpp

---
+++ Graphs Problem Solving/Largest Island.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<vector<int> > v,vector<vector<bool> > &visited,int row,int col,int n,int m){
+    
+    visited[row][col] = true;
+    
+    queue<pair<int,int> > q;
+    q.push({row,col});
+    
+    int count = 1;
+    
+    vector<vector<int> > direc = {{0,1},{0,-1},{-1,0},{1,0}};
+    
+    while(!q.empty()){
+        
+        auto p = q.front();
+        q.pop();
+        
+        for(auto d : direc){
+            int x = p.first + d[0];
+            int y = p.second + d[1];
+            
+            if(x>=0 && x<n && y>=0 && y<m && !visited[x][y] && v[x][y] == 1){
+                count++;
+                q.push({x,y});
+                visited[x][y] = true;
+            }
+        }
+    }
+    return count;
+}
+
+int largest_island(vector<vector<int> > v){
+    //return the size of largest island in grid
+    
+    int n = v.size();
+    int m = v[0].size();
+    
+    vector<vector<bool> > visited(n,vector<bool>(m,false));
+    
+    int count = 0,ans = 0;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(!visited[i][j] && v[i][j] == 1){
+                count = solve(v,visited,i,j,n,m);
+                ans = max(ans,count);
+            }
+        }
+    }
+    return ans;
+}
